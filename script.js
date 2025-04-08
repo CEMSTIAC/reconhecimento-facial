ae0fced6be5057c3502387cdf3e57aa7419dce8a
CEMSTIAC <simone.19333@edu.campos.rj.gov.br>
2025-04-08 16:47:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const video = document.getElementById('video');
 
 async function carregarModelos() {
-  await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
-  await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
-  await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
-  await faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
+ await faceapi.nets.tinyFaceDetector.loadFromUri('models');
+await faceapi.nets.faceLandmark68Net.loadFromUri('models');
+await faceapi.nets.faceRecognitionNet.loadFromUri('models');
+await faceapi.nets.ssdMobilenetv1.loadFromUri('models');
+
 }
 
 async function startVideo() {
